38c297848e44d028a224a4e6e1f78fc3f528a93b
Glenn Dunnegan <glenn.dunnegan@gmail.com>
2023-02-13 06:54:33 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Site is currently under construction.
         </p>
+        <p>Please come back later.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
